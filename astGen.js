edccbfb22a59e2d923d9d4871326217816b2d05f
Adrian Marten <adrian_marten@hotmail.com>
2021-07-26 21:12:52 +0200

    Finished test cases for ArrayExpression transformation

--- astGen.js
+++ astGen.js
@@ -1,8 +1,69 @@
 import * as espree from 'espree';
+import * as escodegen from 'escodegen';
 
-const exp = "var a = [1,2,3,4]";
+const exp = "var a = [[\"a\",\"b\",\"c\",\"d\"], [1,2,3,4], [{hello: \"World\"}, {say: 2}, {return: false}]]";
+const oAST = {
+    type: "ArrayExpression",
+    elements: [
+        {
+            type: "ObjectExpression",
+            properties: [
+                {
+                    type: "Property",
+                    key: {
+                        type: "Identifier",
+                        name: "hello"
+                    },
+                    value: {
+                        type: "Literal",
+                        value: "World",
+                        raw: "\"World\""
+                    },
+                    kind: "init"
+                }
+            ]
+        },
+        {
+            type: "ObjectExpression",
+            properties: [
+                {
+                    type: "Property",
+                    key: {
+                        type: "Identifier",
+                        name: "say"
+                    },
+                    value: {
+                        type: "Literal",
+                        value: 2,
+                        raw: "2"
+                    },
+                    kind: "init"
+                }
+            ]
+        },
+        {
+            type: "ObjectExpression",
+            properties: [
+                {
+                    type: "Property",
+                    key: {
+                        type: "Identifier",
+                        name: "return"
+                    },
+                    value: {
+                        type: "Literal",
+                        value: false,
+                        raw: "false"
+                    },
+                    kind: "init"
+                }
+            ]
+        }
+    ]
+};
 
 const ast = espree.parse(exp);
+const tempContent = escodegen.generate(oAST)
 
 debugger;
 
